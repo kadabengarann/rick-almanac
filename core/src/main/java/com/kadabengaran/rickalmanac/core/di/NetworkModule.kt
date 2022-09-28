9473d7d9f8e664bd635b436ff35941708299ee8e
Ardhy Satrio <rio27dh@gmail.com>
2022-09-28 17:24:00 +0800

    add certificate pinning for connection to server

--- core/src/main/java/com/kadabengaran/rickalmanac/core/di/NetworkModule.kt
+++ core/src/main/java/com/kadabengaran/rickalmanac/core/di/NetworkModule.kt
@@ -5,11 +5,13 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
+import com.kadabengaran.rickalmanac.core.BuildConfig
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -17,17 +19,23 @@ class NetworkModule {
 
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
+        val certificatePinner = CertificatePinner.Builder()
+            .add(BuildConfig.HOST_NAME, BuildConfig.KEY_1)
+            .add(BuildConfig.HOST_NAME, BuildConfig.KEY_2)
+            .add(BuildConfig.HOST_NAME, BuildConfig.KEY_3)
+            .build()
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
     @Provides
     fun provideApiService(client: OkHttpClient): ApiService {
         val retrofit = Retrofit.Builder()
-            .baseUrl("https://rickandmortyapi.com/api/")
+            .baseUrl(BuildConfig.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .client(client)
             .build()
